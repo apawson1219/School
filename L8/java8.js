112f97202137d62e3be414f94ce5ecaf64174e26
apawson1219 <55643699+apawson1219@users.noreply.github.com>
2019-11-09 18:57:27 -0700

    Add files via upload

---
+++ L8/java8.js
@@ -0,0 +1,64 @@
+function askforinput(){
+document.getElementById("name").innerHTML = "What is your Full name?";
+getName();
+document.getElementById("email").innerHTML = "What is your email?";
+getEmail();
+document.getElementById("phone").innerHTML = "What is your phone number?";
+getPhone();
+document.getElementById("zip").innerHTML = "What is your zip code?";
+getZip();
+document.getElementById("date").innerHTML = "What is the date of the storm?";
+getDate();
+document.getElementById("type").innerHTML = "What type of storm?";
+getType();
+document.getElementById("severity").innerHTML = "How sever is it?";
+getSeverity();
+document.getElementById("region").innerHTML = "What region is it in?";
+getRegion();
+document.getElementById("danger").innerHTML = "How dangerous is it?";
+getDanger();
+document.getElementById("comment").innerHTML = "Additional comments";
+getComments();
+
+}
+function getName() {
+
+}
+
+function getEmail() {
+
+}
+
+function getPhone() {
+
+}
+function getZip() {
+
+}
+
+function getDate() {
+
+}
+
+function getType() {
+
+}
+
+function getSeverity() {
+
+}
+
+function getRegion() {
+
+}
+
+function getDanger() {
+
+}
+
+function getComment() {
+
+}
+
+
+
